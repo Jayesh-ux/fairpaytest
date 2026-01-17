7b7bf1e9fca7359b2c0d028e66c52c0e9692b999
Jayesh Singh <hsinghjayesh@gmail.com>
2026-01-17 15:39:16 +0530

    web analytics vercel

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ScrollToTop } from "@/components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import Calculator from "./pages/Calculator";
 import HowItWorks from "./pages/HowItWorks";
@@ -49,6 +50,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
